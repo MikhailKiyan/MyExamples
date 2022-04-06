c9b0b864650cab76e750c498ff70d957e6ebed4b
Michael Kiyan <Mykhailo.Kiian@gmail.com>
2022-04-05 20:55:18 -0700

    Initial commit

---
+++ Benchmark/MyExaples.Benchmark/LoggingBenchmark.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace MyExaples.Benchmark;
+
+[MemoryDiagnoser]
+public class MemoryBenchmarkerDemo {
+    int NumberOfItems = 100_000;
+
+    [Benchmark]
+    public string ConcatStringsUsingStringBuilder() {
+        var sb = new StringBuilder();
+        for (int i = 0; i < NumberOfItems; i++) {
+            sb.Append("Hello World!" + i);
+        }
+        return sb.ToString();
+    }
+
+    [Benchmark]
+    public string ConcatStringsUsingGenericList() {
+        var list = new List<string>(NumberOfItems);
+        for (int i = 0; i < NumberOfItems; i++) {
+            list.Add("Hello World!" + i);
+        }
+        return list.ToString() ?? "";
+    }
+}
