cd0893741ad55980700d95d2bc16578fbc24b7c6
Michael Kiyan <Mykhailo.Kiian@gmail.com>
2022-05-03 20:33:27 -0700

    Added Redis Server and RabbitMQ Server
    Fixed some issues

--- docker-compose/run.cmd
+++ docker-compose/run.cmd
@@ -1,3 +1,4 @@
-docker-compose -p ms-sql-server -f ms-sql-server-start.yml up -d
+docker-compose -p ms-sql-server -f ms-sql-server.yml up -d
 docker-compose -p elastic-search -f elastic-search.yml up -d
---docker-compose -p redis -f redis.yml up -d
+docker-compose -p redis -f redis.yml up -d
+docker-compose -p rabbit-mq-server -f rabbit-mq-server.yml up -d
